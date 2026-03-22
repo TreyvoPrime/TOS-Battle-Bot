49e7fd74e9383ddb0ef149b54e1dec9bff4e6b3a
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-03-22 01:31:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import os
 # Initialize Flask app
 app = Flask(__name__)
 
-# =========================
-# ROUTES
-# =========================
 
 @app.route("/")
 def home():
@@ -21,9 +18,6 @@ def terms():
 def privacy():
     return "Privacy Policy coming soon."
 
-# =========================
-# RUN (only for local dev)
-# =========================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
